2382bb2b0abe6d4236902de312bd33691d524925
AsafDavidov <adavidov@conncoll.edu>
2018-11-28 15:31:44 -0500

    add insertion and selection sorts

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,22 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let sorted = false;
+  for (let i = 0; i < arr.length; i++) {
+    let curIndex = i
+    let j=i-1
+    while (!sorted&&i>0) {
+      if(arr[curIndex]<arr[j]){
+        temp=arr[curIndex];
+        arr[curIndex]=arr[j];
+        arr[j]=temp;
+        curIndex--
+        j--
+      }else{
+        console.log("farts");
+        sorted=true
+      }
+    }
+    sorted=false
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,18 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let minIndex;
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    minIndex=i
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j]<arr[minIndex]){
+        minIndex=j
+      }
+    }
+    temp=arr[i];
+    arr[i]=arr[minIndex];
+    arr[minIndex]=temp;
+  }
+
 }
